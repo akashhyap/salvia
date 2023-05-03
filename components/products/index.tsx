02b2b9811caff08cfd3bcbb008250814de18efe4
Akashdeep <akash.kashhyap@gmail.com>
2023-05-03 18:29:20 +0530

    changing product grid

--- components/products/index.tsx
+++ components/products/index.tsx
@@ -25,7 +25,7 @@ const Products = ({ products }: ProductsProps) => {
 
   return (
     <div className="px-4 py-10 lg:px-6 lg:py-14 mx-auto">
-      <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-6 mb-10 lg:mb-14">
+      <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-10 lg:mb-14">
         {products?.edges.map((product) => {
           return <Product key={product.node?.id} product={product} />;
         })}
