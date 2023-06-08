8f9d10a5875ed3ee57c1a6f67d273fcc6c82b670
Akashdeep <akash.kashhyap@gmail.com>
2023-06-08 17:16:19 +0530

    article image update

--- components/Article.tsx
+++ components/Article.tsx
@@ -1,4 +1,5 @@
 import { StoryblokComponent } from '@storyblok/react';
+import Image from 'next/image';
 import { render } from 'storyblok-rich-text-react-renderer';
 
 // @ts-ignore
@@ -8,11 +9,20 @@ const Article = ({ blok }) => {
   return (
     <section className="text-gray-600 body-font">
       <div className="container mx-auto flex px-5 py-14 items-center justify-center flex-col">
-        <img
+        {/* <img
           className="md:h-96 w-full mb-10 object-cover object-center rounded"
           alt={blok.image.alt}
           src={blok.image.filename}
-        />
+        /> */}
+        <figure className='relative md:h-96 w-full mb-10'>
+          <Image
+            alt={blok.image.alt}
+            src={blok.image.filename}
+            layout='fill'
+            objectFit='cover'
+            objectPosition='center top'
+          />
+        </figure>
         <div className="lg:w-2/3 w-full">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
             {blok.title}
